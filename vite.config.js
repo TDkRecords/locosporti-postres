358874fdabaf04527445258f0c7f2d9f3170197b
TDkRecords <tdksongs@gmail.com>
2026-08-12 23:03:37 -0500

    porfin funciona el build pues el problema era vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules')
-						? undefined
-						: true
-			}
-		})
+		sveltekit()
 	]
 });
