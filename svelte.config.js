4c45a1495530fae33dd766966476801a7c4bf55d
TDkRecords <tdksongs@gmail.com>
2026-08-12 21:59:19 -0500

    adaptadores v4

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const useStatic = process.env.ADAPTER === 'static';
 
 export default {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter()
+        adapter: useStatic
+            ? adapterStatic({
+                pages: 'build',
+                assets: 'build',
+                fallback: 'index.html'
+            })
+            : adapterVercel()
     }
 };
